40ba9aa0dfee6645625410390342fbf3b31a8f74
tcheuD <tcheutcheud@gmail.com>
2020-09-10 10:18:25 +0200

    13 - DQL & The Query Builder

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -22,13 +22,11 @@ class QuestionRepository extends ServiceEntityRepository
     /**
     * @return Question[] Returns an array of Question objects
     */
-    public function findAllAskedOrderedByNewest($value)
+    public function findAllAskedOrderedByNewest()
     {
         return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('q.askedAt IS NOT NULL')
+            ->orderBy('q.askedAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;
