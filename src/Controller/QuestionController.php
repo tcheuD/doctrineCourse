74d19ff95cf63a1dfa6b89b6cf12fbe717006677
tcheuD <tcheutcheud@gmail.com>
2020-09-10 10:29:22 +0200

    15 - Automatic Controller Queries: Param Converter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -73,20 +73,11 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
-
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
-
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
-        }
-
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
